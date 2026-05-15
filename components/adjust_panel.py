83c3315eddd91c2b7f79f198ed7b249e544ee1c2
Huyen Tran <huyentranht05@gmail.com>
2026-05-15 02:57:39 +0000

    fix left panel va main canvas

--- components/adjust_panel.py
+++ components/adjust_panel.py
@@ -1,19 +1,27 @@
 import streamlit as st
 from services import *
+from PIL import Image 
 import os
 
 def display_left_panel():
     embed_css("adjust_panel.css")
     with st.container(key ="left_panel"):
         st.subheader("TOOLS", anchor= False)
-        st.file_uploader(
+
+        img = st.file_uploader(
             label= "UPLOAD IMAGE",
-            type = ["pnj","jpg","jpeg"],
+            type = ["png","jpg","jpeg"],
             accept_multiple_files= False,
             label_visibility="collapsed"
         )
 
-        if st.button("AI ANALYZE",use_container_width=True, key = 'btn_ai'):
+        if img is not None:
+            img = Image.open(img).copy()
+
+            st.session_state.original_img = img
+            st.session_state.edited_img = img.copy()
+
+        if st.button("AUTO FIX",use_container_width=True, key = 'btn_ai'):
             pass
         if st.button("REMOVE OBJECT",  icon="🧽", use_container_width=True, key = 'btn_remove'):
             pass
